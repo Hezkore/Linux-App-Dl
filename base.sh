0cdad96a6aa80eb3aabd2e6a684ab340b6e8fe2e
hezkore <hezkore@gmail.com>
2022-07-03 00:23:06 +0200

    test

--- base.sh
+++ base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # -*- Mode: sh; coding: utf-8; indent-tabs-mode: t; tab-width: 4 -*-
 
-notify-send "Installing ${1}"
+notify-send -e "Installing ${1}"
 
 wget --no-cache -O - https://raw.githubusercontent.com/Hezkore/Linux-App-Dl/master/Debian/${1}.sh | sh -
