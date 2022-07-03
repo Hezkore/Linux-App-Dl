1fb43de52d164360d9576166b33de11904ca80e1
hezkore <hezkore@gmail.com>
2022-07-03 15:45:20 +0200

    test

---
+++ Debian/lutris.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# -*- Mode: sh; coding: utf-8; indent-tabs-mode: t; tab-width: 4 -*-
+
+sudo add-apt-repository ppa:lutris-team/lutris -y
+sudo apt install lutris -y
+
+end_with "lutris"
