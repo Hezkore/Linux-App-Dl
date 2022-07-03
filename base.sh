0250f59273fee31a51078059e29642b4b46b4e33
hezkore <hezkore@gmail.com>
2022-07-03 02:36:45 +0200

    test

--- base.sh
+++ base.sh
@@ -38,7 +38,7 @@ while ! check_pass; do
 		#source "./Debian/htop.sh"
 		zenity --progress --width=400 --height=100 --title="Installing ${1}" --text "Installing..." --auto-close --pulsate
 		rm -r "$HOME/.cache/y.ins"
-		$(cat "$HOME/.cache/y.run")
+		eval $(cat "$HOME/.cache/y.run")
 		rm -r "$HOME/.cache/y.run"
 		exit
 	else
